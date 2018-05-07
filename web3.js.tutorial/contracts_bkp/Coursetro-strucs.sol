0a77f5915138193ff82e439f88a3f25fec06d818
Jaime Guzmán <jguzman@ratslabs.io>
2018-05-06 22:16:23 -0300

    Mapping and structs tutorial

---
+++ web3.js.tutorial/contracts_bkp/Coursetro-strucs.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.4.18;
+
+contract Coursetro {
+
+    struct Instructor{
+        uint age;
+        string fName;
+        string lName;
+        
+        
+    }
+
+    mapping (address => Instructor ) instructors;
+ 
+    address[] public instructorsAccts;
+
+    function setInstructor( address _address, uint _age, string _fName, string _lName ) public {
+        
+        var instructor = instructors[_address];
+
+        instructor.age   = _age;
+        instructor.fName = _fName;
+        instructor.lName = _lName;
+
+        instructorsAccts.push( _address ) -1;
+    }  
+
+
+    function getInstructors() view public returns(address[]) {
+        return instructorsAccts;
+    }
+    
+    function getInstructors(address _address) view public returns (uint, string, string ){
+        return (instructors[_address].age, instructors[_address].fName , instructors[_address].lName);
+    }    
+    
+    function countInstructor() view public returns (uint){
+        return instructorsAccts.length;
+    }
+}
